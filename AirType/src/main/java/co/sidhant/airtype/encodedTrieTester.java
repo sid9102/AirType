7d9ac2e1925333d8fd25c8100b1816ac1a6613d4
Sidhant Srikumar <sidhant.srikumar@utexas.edu>
2013-12-14 03:05:18 -0600

    Tester is done!
    I have now conclusively shown that the EncodedTrie is substantially
    smaller in memory than AirTrie. Traversing it is still constant time,
    although slower than AirTrie, it is negligible. Getting 2,000,000
    characters worth of words would take 0.8 seconds.

--- AirType/src/main/java/co/sidhant/airtype/encodedTrieTester.java
+++ AirType/src/main/java/co/sidhant/airtype/encodedTrieTester.java
@@ -1,6 +1,8 @@
 package co.sidhant.airtype;
 
 
+import net.sourceforge.sizeof.SizeOf;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -45,37 +47,61 @@ public class EncodedTrieTester
         }
 
         // Make the trie
-        AirTrie curTrie = TrieMaker.makeTrie(new FingerMap(), permutationMap);
-        EncodedTrie encodedTrie = new EncodedTrie(curTrie, null);
-        // Comparing outputs from the encoded and normal trie
-        Random rand = new Random();
         int iterations = 100;
-        AirTrieNode curNode;
+        AirTrie curTrie = TrieMaker.makeTrie(new FingerMap(), permutationMap);;
+        EncodedTrie encodedTrie = new EncodedTrie(curTrie, null);;
+        long ETSize = 0;
+        long ATSize = 0;
         for(int i = 0; i < iterations; i++)
         {
-            // First, do things the normal way
-            curNode = curTrie.root;
+            curTrie = TrieMaker.makeTrie(new FingerMap(), permutationMap);
+            encodedTrie = new EncodedTrie(curTrie, null);
+            ATSize += SizeOf.deepSizeOf(curTrie);
+            ETSize += SizeOf.deepSizeOf(encodedTrie);
+        }
+        ETSize /= iterations * 1000000;
+        ATSize /= iterations * 1000000;
+        System.out.println("AirTrie is, on average, " + ATSize + " MB.");
+        System.out.println("EncodedTrie is, on average, " + ETSize + " MB.");
+        // Comparing outputs from the encoded and normal trie
+        Random rand = new Random();
+        AirTrieNode curNode;
+        long ATStart = 0;
+        long ETStart = 0;
+        long ETTotal = 0;
+        long ATTotal = 0;
+        long totalLetters = 0;
+        for(int i = 0; i < iterations; i++)
+        {
             int wordLength = rand.nextInt(21);
+            totalLetters += wordLength;
             String input = "";
             // Simulate keypresses
             for(int j = 0; j < wordLength; j++)
             {
                 input += Integer.toString(rand.nextInt(8));
             }
-            System.out.println("For input " + input + ";");
+            //System.out.println("For input " + input + ";");
 
             String output = "";
+            ATStart = System.nanoTime();
+            // First, do things the normal way
+            curNode = curTrie.root;
             for(int j = 0; j < wordLength; j++)
             {
+                ATStart = System.nanoTime();
                 int index = Integer.parseInt(input.substring(j, j + 1));
                 curNode = curNode.getChild(index);
                 if(curNode == null)
                     break;
                 output = curNode.getWord();
-                System.out.println(curNode.index + " " + output);
+                //System.out.println(curNode.index + " " + output);
             }
-            System.out.println("AirTrie got the word " + output + ".");
+            //System.out.println("AirTrie got the word " + output + ".");
+            ATTotal += System.nanoTime() - ATStart;
 
+            // Now provide the same input to encodedTrie
+            ETStart = System.nanoTime();
             encodedTrie.resetCurNode();
             for(int j = 0; j < wordLength; j++)
             {
@@ -83,13 +109,19 @@ public class EncodedTrieTester
                 if(encodedTrie.goToChild(index))
                 {
                     output = encodedTrie.getWord();
-                    System.out.println(encodedTrie.curNodeIndex + " " + output);
+                    //System.out.println(encodedTrie.curNodeIndex + " " + output);
                 }
                 else
                     break;
             }
-            System.out.println("EncodedTrie got the word " + output + ".");
+            ETTotal += System.nanoTime() - ETStart;
+            //System.out.println("EncodedTrie got the word " + output + ".");
         }
+
+        long ETAvg = ETTotal / (totalLetters * iterations);
+        long ATAvg = ATTotal / (totalLetters * iterations);
+        System.out.println("AirTrie took an average of " + ATAvg + " nanoseconds per character");
+        System.out.println("EncodedTrie took an average of " + ETAvg + " nanoseconds per character");
     }
 
 }
