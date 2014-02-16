f64d58990494a97d689e3f2d6fac95a149ba6d2d
Sidhant Srikumar <sidhant.srikumar@utexas.edu>
2014-02-16 17:21:06 -0600

    updated arduino code for data collection

--- Arduino/eight_stretch_sensors_java/eight_stretch_sensors_java.ino
+++ Arduino/eight_stretch_sensors_java/eight_stretch_sensors_java.ino
@@ -4,7 +4,7 @@ String breaker = "END";
 int values[10];
 int oldValues[10];
 unsigned long time;
-  
+boolean go = false;  
 void setup()
 {
   Serial.begin(9600);             // Setup serial
@@ -20,13 +20,17 @@ void loop()
     values[5] = analogRead(A2);
     values[6] = analogRead(A1);
     values[7] = analogRead(A0);
-    time = millis(); // Store the current time since the program began
+    //time = millis(); // Store the current time since the program began
   
     String data = "";
     //Prints the time in ms since the program started running.
-    
-    data.concat(time);
-    data.concat(":");
+    if (Serial.available() > 0)
+    {
+      go = !go;
+      Serial.read();
+    }
+//    data.concat(time);
+//    data.concat(":");
     // print out value over the serial port    
     for(int i = 0; i < 8; i++)
     {
@@ -36,8 +40,9 @@ void loop()
       }
       oldValues[i] = values[i];
     }
+    if(go){
     Serial.println(data);
-    
+    }
     // wait for a bit to not overload the port
     delay(100);
 }
