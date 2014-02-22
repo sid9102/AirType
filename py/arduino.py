f31767f5acea95fc9a4464039931de528b69a79f
SaggySanctum <benjamin.d.brown@utexas.edu>
2014-02-21 18:57:57 -0800

    Changes to arduino.py

--- py/arduino.py
+++ py/arduino.py
@@ -14,20 +14,68 @@ def scale(x):
 
 rectangles = [pygame.Rect(0, 0, 100, 100), pygame.Rect(0, 125, 100, 100), pygame.Rect(0, 250, 100, 100), pygame.Rect(0, 375, 100, 100)]
 
-
 def getrandomcolor():
     return pygame.Color(random.randrange(255), random.randrange(240), random.randrange(240), 255)
 
 colors = [getrandomcolor(), getrandomcolor(), getrandomcolor(), getrandomcolor()]
 
+def process_serial(currtime, valdict):
+    values = ser.readline()[:-2].split("\t")
+    valtime = int(values[0])
+    while(valtime <= currtime):
+        valdict[valtime] = values
+    return values
+
+def process_keys(currtime, keydict, valdict):
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.event.post(pygame.event.Event(QUIT))
+            else:
+                keydict[currtime] = event.key
+        elif event.type == QUIT:
+            assdict = generate_asoc(keydict, valdict)
+            pickle.dump(assdict, open("ass.data", 'wb'))
+            pygame.quit()
+            sys.exit(0)
+    return "No key pressed"
+
+def generate_asoc(keydict, valdict):
+    assdict = dict
+    for keytime in keydict.keys():
+    closestdist = 9999999
+    closesttime = -1
+    for valtime in valdict.keys():
+        if abs(valtime - keytime) < closestdist:
+            closestdist = abs(valtime - keytime)
+            closesttime = valtime
+    collide(assdict, keydict[keytime], valdict[closesttime])
+    return assdict    
+
+def collide(assdict, key, newvalues):
+    if key in assdict:
+        oldvalues = assdict[key]
+        for x in range(len(oldvalues)):
+            oldvalues[x] = (oldvalues[x] + int(newvalues[x])) / 2
+    else:
+        oldvalues = []
+        for x in range(len(values)):
+            oldvalues[x] = int(newvalues[x])
+
+valdict = dict
+keydict = dict
 values = []
 time.sleep(1)
 millis = int(round(time.time() * 1000)) - start
 ser.write(str(millis))
 while True:
-    values = ser.readline()[:-2].split("\t")
+    # read shit
     millis = int(round(time.time() * 1000)) - start
-    print(str(values[0]) + " " + str(millis))
+    values = process_serial(millis, valdict)
+    key = process_keys(millis, keydict, valdict)
+    print(str(key) + " " + str(values[0]) + " " + str(millis))
+
+    # draw shit
     windowSurface.fill(pygame.Color(255, 255, 255, 255))
     if len(values) == 5:
         for x in range(1, 5):
@@ -37,12 +85,5 @@ while True:
             textRect = textSurface.get_rect()
             textRect.topleft = rectangles[x-1].topleft
             windowSurface.blit(textSurface, textRect)
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                pygame.event.post(pygame.event.Event(QUIT))
-        elif event.type == QUIT:
-            pygame.quit()
-            sys.exit(0)
     pygame.display.update()
     fpsClock.tick(30)
