7b2014055b29a0c76f50e09979d50f1c634023da
Michael Pfister <pfista@gmail.com>
2014-03-07 21:10:16 -0600

    Started PythonInterface for accessing the encoded trie from python

--- AirType/src/main/java/desktop/PythonInterface.java
+++ AirType/src/main/java/desktop/PythonInterface.java
@@ -1,6 +1,17 @@
 package desktop;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+import io.airtype.AirTrie;
+import io.airtype.AirTypeIME;
+import io.airtype.EncodedTrie;
 import io.airtype.FingerMap;
+import io.airtype.TrieMaker;
+import py4j.GatewayServer;
 
 /**
  * Created by sid9102 on 3/4/14.
@@ -10,9 +21,62 @@ import io.airtype.FingerMap;
 
 public class PythonInterface
 {
-    public static void main(String[] args)
-    {
-        FingerMap fMap = new FingerMap(true);
+    private TreeMap<String, ArrayList<String>> permutationMap;
+    private EncodedTrie mEncodedTrie;
 
+    public PythonInterface() {
+        // Make the map
+        Scanner s = null;
+        try {
+            s = new Scanner(new File("permutations.txt"));
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.exit(0);
+        }
+        permutationMap = new TreeMap<String, ArrayList<String>>(new AirTypeIME.stringLengthComparator());
+        String curLine;
+        String curKey = "";
+        ArrayList<String> curList;
+        while (s.hasNextLine()) {
+            curLine = s.nextLine();
+            if (curLine.endsWith(":")) {
+                curKey = curLine.substring(0, curLine.length() - 1);
+                curList = new ArrayList<String>();
+                curList.add(s.nextLine());
+                permutationMap.put(curKey, curList);
+            } else {
+                curList = permutationMap.get(curKey);
+                curList.add(curLine);
+                permutationMap.put(curKey, curList);
+            }
+        }
+
+        // Make the trie
+        AirTrie airTrie = TrieMaker.makeTrie(new FingerMap(false), permutationMap);
+        mEncodedTrie = new EncodedTrie(airTrie, null);
+
+    }
+
+    public EncodedTrie getEncodedTrie() {
+        return mEncodedTrie;
+    }
+
+    public String getWord(String digits) {
+        String result = "";
+        mEncodedTrie.resetCurNode();
+        for (int j = 0; j < digits.length(); j++) {
+            int index = Integer.parseInt(digits.substring(j, j + 1));
+            if (mEncodedTrie.goToChild(index)) {
+                result = mEncodedTrie.getWord();
+            } else
+                break;
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        GatewayServer gs = new GatewayServer(new PythonInterface(), 25346);
+        gs.start();
     }
 }
